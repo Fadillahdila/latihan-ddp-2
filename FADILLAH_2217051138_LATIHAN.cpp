0e448f3d0b4ecf58eff28a2415b964c7e46214cd
Fadillahdila <112566187+Fadillahdila@users.noreply.github.com>
2022-09-09 20:01:28 +0700

    Add files via upload

---
+++ FADILLAH_2217051138_LATIHAN.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+	float phi = 3.14;
+	float r = 7;
+	float L = phi*r*r;
+	
+	cout<<"luas lingkaran dengan jari-jari "<<r<<" adalah "<<L;
+	return 0;
+}
